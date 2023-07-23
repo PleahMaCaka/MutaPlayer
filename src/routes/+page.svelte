596246c2f09072e03c8dc3d58fe8468f084b7b3f
pleahmacaka <pleahmacaka@gmail.com>
2023-07-23 23:30:55 +0900

    refactor: dir name as lower

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import { initPlayer } from "$lib/api/InitPlayer"
     import { togglePlayer } from "$lib/api/shortcuts/ToggleWindow"
     import Background from "$lib/components/Background.svelte"
-    import Player from "$lib/components/Player/Player.svelte"
+    import Player from "$lib/components/player/Player.svelte"
     import { register, unregister } from "@tauri-apps/api/globalShortcut"
     import { onMount } from "svelte"
 
