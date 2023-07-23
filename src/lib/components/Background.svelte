81510d09a5be8a35b3e76263eaa2e7738ded859e
pleahmacaka <pleahmacaka@gmail.com>
2023-07-23 23:47:45 +0900

    chore: bg codes make clean

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -1,43 +1,34 @@
 <script>
+    import "../components/styles/MoveAnimations.css"
+
     let borderRadius = 25
 </script>
 
-<div id="player-bg-holder">
-    <div id="bg-wrapper"
-         style="--border-radius: {borderRadius}px"
-    >
-        <div id="bg-image"></div>
-        <div id="bg-content-wrapper">
-            <slot/>
-        </div>
+<div id="bg-wrapper"
+     style="--border-radius: {borderRadius}px"
+>
+    <div id="bg-image"></div>
+    <div id="bg-content-wrapper">
+        <slot/>
     </div>
 </div>
 
 
 <style>
-    #player-bg-holder {
-        display: flex;
-        justify-content: center;
-        margin: 0 auto;
-    }
-
     #bg-content-wrapper {
+        width: 99%;
+        height: 99%;
+        padding: 0.4% 0.4% 0 0.4%;
         position: absolute;
-        width: 100%;
-        height: 100%;
-
-        animation: slideDown 1s ease-in-out;
-        animation-fill-mode: forwards;
     }
 
     #bg-wrapper {
-        position: fixed;
+        position: absolute;
         width: 100%;
         height: 100%;
         overflow: hidden;
-
-        border-radius: var(--border-radius);
         box-shadow: inset 0 0 0 1px #BE81FFFF;
+        border-radius: var(--border-radius);
 
         /* TOP-DOWN mode, if reverse this: Uwu  */
         /*border-radius: 0 0 var(--border-radius) var(--border-radius);*/
@@ -45,22 +36,13 @@
 
         animation: slideDown 1s ease-in-out;
         animation-fill-mode: forwards;
-    }
 
-    @keyframes slideDown {
-        0% {
-            transform: translateY(-100%);
-        }
-        100% {
-            transform: translateY(0%);
-        }
     }
 
     #bg-image {
         position: absolute;
         width: 100%;
         height: 100%;
-        background-color: rgba(80, 80, 80, 0.85);
         background-image: url(https://cdn.discordapp.com/attachments/966760602720665720/1131937730507579452/example.png);
         z-index: -100;
         filter: blur(50px) brightness(0.75);
